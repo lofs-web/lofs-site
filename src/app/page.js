c502b778ff8b03c3b75d7f0944fd7a4e74eab248
alex <alex@alexs-MacBook-Pro-2.local>
2025-11-04 20:00:05 +0530

    Force rebuild to update client component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
 import Home from "./page.client";
 
+// Force rebuild to pick up client changes
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return <Home />;
 }
-// force Vercel rebuild
